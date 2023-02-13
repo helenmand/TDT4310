c1d5b67852b1a8d86a012e7df8c2095998d93ad6
Tollef Jørgensen <tollefj@gmail.com>
2023-02-13 19:31:49 +0100

    added download-script for sentiment140

---
+++ data/sentiment140/download.sh
@@ -0,0 +1,5 @@
+wget http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip
+unzip trainingandtestdata.zip
+mv training.1600000.processed.noemoticon.csv train.csv
+mv testdata.manual.2009.06.14.csv test.csv
+rm trainingandtestdata.zip
