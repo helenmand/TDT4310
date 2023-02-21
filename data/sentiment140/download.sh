ce43e347581dde24a7eb93054a4cbb27be43dcc4
Tollef Jørgensen <tollefj@gmail.com>
2023-02-21 17:36:21 +0100

    remove sentiment140 in favor of amazon data

--- data/sentiment140/download.sh
+++
@@ -1,5 +0,0 @@
-wget http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip
-unzip trainingandtestdata.zip
-mv training.1600000.processed.noemoticon.csv train.csv
-mv testdata.manual.2009.06.14.csv test.csv
-rm trainingandtestdata.zip
