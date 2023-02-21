b5b27fe96e3ceb6bd77c627c2e2322f158ce286b
Tollef Jørgensen <tollefj@gmail.com>
2023-02-21 17:35:53 +0100

    parsing of original amazon set

--- data/amazon_reviews/parse_amazon_reviews.py
+++ data/amazon_reviews/parse_amazon_reviews.py
@@ -6,10 +6,17 @@ data = "amazon_reviews_us_Major_Appliances_v1_00.tsv"
 df = pd.read_csv(data, sep='\t', error_bad_lines=False)
 df.head()
 # %%
-df.columns
-# %%
-# filter columns
 columns_to_keep = ['star_rating', 'helpful_votes', 'total_votes', 'verified_purchase', 'review_headline', 'review_body']
 df = df[columns_to_keep]
 df.head()
 df.to_csv('amazon_appliances_reviews.csv', index=False)
+# %%
+df.describe()
+df.nunique()
+
+import matplotlib.pyplot as plt
+stars = df['star_rating'].value_counts()
+plt.bar(stars.index, stars.values)
+plt.xlabel('Star Rating')
+plt.ylabel('Count')
+plt.show()
